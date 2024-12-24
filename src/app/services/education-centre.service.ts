e4e725b51e5576fdb51eda1e505cedf4cef0eb41
Aravind <aravindmuthiah7@gmail.com>
2024-12-24 22:20:28 +0530

    Responsive css

--- src/app/services/education-centre.service.ts
+++ src/app/services/education-centre.service.ts
@@ -8,13 +8,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class EducationCentreService {
   constructor(private http: HttpClient) {}
-
+  
+  getCentresURL!: string;
   private mockCentres: EducationCentre[] = [];
 
   getCentres() {
     console.log(JSON.stringify(this.mockCentres));
+    this.getCentresURL = 'https://run.mocky.io/v3/b02f4f2b-408b-4b90-bea5-a91a1ffa57c5';
     const headers = new HttpHeaders().set('Accept', 'application/json');
-    this.http.get<EducationCentre[]>('https://run.mocky.io/v3/b02f4f2b-408b-4b90-bea5-a91a1ffa57c5', { headers }).subscribe(x => {
+    this.http.get<EducationCentre[]>(this.getCentresURL, { headers }).subscribe(x => {
       console.log('bv centres in cmse are: ', x );
       this.mockCentres = x;
     })
