eba4adbf71d840d33d90ad43dd147e520c34986b
Aravind <aravindmuthiah7@gmail.com>
2025-01-03 13:01:39 +0530

    Fixing the runmocky issue & adding new data

--- src/app/services/education-centre.service.ts
+++ src/app/services/education-centre.service.ts
@@ -14,7 +14,7 @@ export class EducationCentreService {
 
   getCentres() {
     console.log(JSON.stringify(this.mockCentres));
-    this.getCentresURL = 'https://run.mocky.io/v3/b02f4f2b-408b-4b90-bea5-a91a1ffa57c5';
+    this.getCentresURL = 'https://run.mocky.io/v3/abd7c9d1-d50e-4e71-aeaf-4b2f01ce9243';
     const headers = new HttpHeaders().set('Accept', 'application/json');
     this.http.get<EducationCentre[]>(this.getCentresURL, { headers }).subscribe(x => {
       console.log('bv centres in cmse are: ', x );
@@ -24,7 +24,7 @@ export class EducationCentreService {
 
   searchCentres(area: string, pincode: string): Observable<EducationCentre[]> {
     return of(this.mockCentres.filter(centre => 
-      centre.area.toLowerCase().includes(area.toLowerCase()) &&
+      centre.area?.toLowerCase().includes(area?.toLowerCase()) &&
       centre.pincode.includes(pincode)
     ));
   }
