9199adb8a3c9abba583836c965019a8aacf9440a
Aravind <aravindmuthiah7@gmail.com>
2025-11-08 12:59:21 +0530

    Fixed Routing and Redirect issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { AppComponent } from './app.component';
-import { authGuard } from './services/auth.guard';
-import { SearchFormComponent } from './components/search-form/search-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, //main content - locator form 
-  { path: 'home', component: AppComponent },
-  { path: 'search', component: SearchFormComponent },
-  { path: 'login', component: LoginComponent },
-  // { path: '**', redirectTo: '' }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },  
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
