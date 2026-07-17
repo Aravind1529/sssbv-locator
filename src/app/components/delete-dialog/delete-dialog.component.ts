fd2d6d5cda257b33031690ac506126a03c14d6e6
Aravind <aravindmuthiah7@gmail.com>
2026-07-17 13:57:50 +0530

    Revert "Merge pull request #15 from Aravind1529/release-dev"
    This reverts commit 5f13b7dc10fdb18b87cb4d0e4469200cfe044560, reversing
    changes made to 60b23b9f7760fa7f4f3ab3fff4a490a04f4b1338.

--- src/app/components/delete-dialog/delete-dialog.component.ts
+++ src/app/components/delete-dialog/delete-dialog.component.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ToastrService } from 'ngx-toastr';
 import { EducationCentreService } from '../../services/education-centre.service';
 
 @Component({
@@ -27,25 +26,12 @@ export class DeleteDialogComponent {
 constructor(
     public dialogRef: MatDialogRef<DeleteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private educationCentreService: EducationCentreService,
-    private toastr: ToastrService
+    private educationCentreService: EducationCentreService
   ) {}
 
   async confirmDelete() {
     console.log(this.data);
     await this.educationCentreService.deleteCentre(this.data.id);
-    
-    // Subscribe to delete message
-    this.educationCentreService.deleteMessage$.subscribe((message) => {
-      if (message) {
-        if (message.type === 'success') {
-          this.toastr.success(message.message);
-        } else {
-          this.toastr.error(message.message);
-        }
-      }
-    });
-    
     this.dialogRef.close(true); // send confirmation
   }
   cancel() {
